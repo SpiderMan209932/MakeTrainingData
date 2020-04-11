455cd156d412261e670b501d927432266f049bae
Hiroki Inoshita <jojo19980302@gmail.com>
2020-04-11 15:12:35 +0900

    画像の表示

--- MakeTrainingData.py
+++ MakeTrainingData.py
@@ -7,13 +7,27 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtCore    import pyqtSlot
+import os
+import cv2
+import mayavi
+import numpy as np
 
 class Ui_MainWindow(object):
+    def __init__(self):
+        self.image_file_name = None
+        self.pointcloud_file_naem = None
+        self.label2d_file_name = None
+        self.label_file_name = None
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1280, 784)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
+        # サイズ
         self.SIzeHeight = QtWidgets.QDoubleSpinBox(self.centralwidget)
         self.SIzeHeight.setGeometry(QtCore.QRect(900, 20, 211, 31))
         self.SIzeHeight.setObjectName("SIzeHeight")
@@ -23,6 +37,7 @@ class Ui_MainWindow(object):
         self.SizeLength = QtWidgets.QDoubleSpinBox(self.centralwidget)
         self.SizeLength.setGeometry(QtCore.QRect(900, 80, 211, 31))
         self.SizeLength.setObjectName("SizeLength")
+        # 位置
         self.PositionX = QtWidgets.QDoubleSpinBox(self.centralwidget)
         self.PositionX.setGeometry(QtCore.QRect(900, 110, 211, 31))
         self.PositionX.setObjectName("PositionX")
@@ -32,17 +47,20 @@ class Ui_MainWindow(object):
         self.PositionZ = QtWidgets.QDoubleSpinBox(self.centralwidget)
         self.PositionZ.setGeometry(QtCore.QRect(900, 170, 211, 31))
         self.PositionZ.setObjectName("PositionZ")
+        # 向き
         self.AngleYaw = QtWidgets.QDoubleSpinBox(self.centralwidget)
         self.AngleYaw.setGeometry(QtCore.QRect(900, 200, 211, 31))
         self.AngleYaw.setObjectName("AngleYaw")
+
+        # 文字
+        self.TextHeight = QtWidgets.QLineEdit(self.centralwidget)
+        self.TextHeight.setGeometry(QtCore.QRect(860, 20, 41, 31))
+        self.TextHeight.setAlignment(QtCore.Qt.AlignCenter)
+        self.TextHeight.setObjectName("TextHeight")
         self.TextWidth = QtWidgets.QLineEdit(self.centralwidget)
         self.TextWidth.setGeometry(QtCore.QRect(860, 50, 41, 31))
         self.TextWidth.setAlignment(QtCore.Qt.AlignCenter)
         self.TextWidth.setObjectName("TextWidth")
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(860, 20, 41, 31))
-        self.lineEdit.setAlignment(QtCore.Qt.AlignCenter)
-        self.lineEdit.setObjectName("lineEdit")
         self.TextLength = QtWidgets.QLineEdit(self.centralwidget)
         self.TextLength.setGeometry(QtCore.QRect(860, 80, 41, 31))
         self.TextLength.setAlignment(QtCore.Qt.AlignCenter)
@@ -63,6 +81,8 @@ class Ui_MainWindow(object):
         self.TextYaw.setGeometry(QtCore.QRect(860, 200, 41, 31))
         self.TextYaw.setAlignment(QtCore.Qt.AlignCenter)
         self.TextYaw.setObjectName("TextYaw")
+
+        # クラス
         self.ObjecClass = QtWidgets.QComboBox(self.centralwidget)
         self.ObjecClass.setGeometry(QtCore.QRect(860, 240, 251, 31))
         self.ObjecClass.setObjectName("ObjecClass")
@@ -72,24 +92,34 @@ class Ui_MainWindow(object):
         self.ObjecClass.addItem("")
         self.ObjecClass.addItem("")
         self.ObjecClass.addItem("")
+
+        # View
+        self.PointCloudView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.PointCloudView.setGeometry(QtCore.QRect(10, 10, 711, 641))
+        self.PointCloudView.setObjectName("PointCloudView")
+        self.ImageView = QtWidgets.QLabel(self.centralwidget)
+        self.ImageView.setGeometry(QtCore.QRect(730, 290, 531, 361))
+        self.ImageView.setObjectName("ImageView")
+        pixmap = QtGui.QPixmap(self.image_file_name)
+        self.Image = QtGui.QImage()
+        self.ImageView.setPixmap(QtGui.QPixmap.fromImage(self.Image))
+        # view.addItem(self._imageItem)
+
+        # ボタン
         self.NextFrame = QtWidgets.QPushButton(self.centralwidget)
         self.NextFrame.setGeometry(QtCore.QRect(360, 670, 121, 61))
         self.NextFrame.setObjectName("NextFrame")
         self.BeforeFrame = QtWidgets.QPushButton(self.centralwidget)
         self.BeforeFrame.setGeometry(QtCore.QRect(220, 670, 121, 61))
         self.BeforeFrame.setObjectName("BeforeFrame")
-        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(10, 10, 711, 641))
-        self.graphicsView.setObjectName("graphicsView")
-        self.graphicsView_2 = QtWidgets.QGraphicsView(self.centralwidget)
-        self.graphicsView_2.setGeometry(QtCore.QRect(730, 290, 531, 361))
-        self.graphicsView_2.setObjectName("graphicsView_2")
         self.Before2DBox = QtWidgets.QPushButton(self.centralwidget)
         self.Before2DBox.setGeometry(QtCore.QRect(880, 670, 121, 61))
         self.Before2DBox.setObjectName("Before2DBox")
-        self.NextFrame_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.NextFrame_2.setGeometry(QtCore.QRect(1020, 670, 121, 61))
-        self.NextFrame_2.setObjectName("NextFrame_2")
+        self.Next2DBox = QtWidgets.QPushButton(self.centralwidget)
+        self.Next2DBox.setGeometry(QtCore.QRect(1020, 670, 121, 61))
+        self.Next2DBox.setObjectName("Next2DBox")
+
+        # メニューバー
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1280, 28))
@@ -102,6 +132,7 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
         self.OpenFile = QtWidgets.QAction(MainWindow)
         self.OpenFile.setObjectName("OpenFile")
+        self.OpenFile.triggered.connect(self.OpenDialog)
         self.SaveFile = QtWidgets.QAction(MainWindow)
         self.SaveFile.setObjectName("SaveFile")
         self.menuFile.addAction(self.OpenFile)
@@ -115,7 +146,7 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.TextWidth.setText(_translate("MainWindow", "W"))
-        self.lineEdit.setText(_translate("MainWindow", "H"))
+        self.TextHeight.setText(_translate("MainWindow", "H"))
         self.TextLength.setText(_translate("MainWindow", "L"))
         self.TextX.setText(_translate("MainWindow", "X"))
         self.TextY.setText(_translate("MainWindow", "Y"))
@@ -130,13 +161,40 @@ class Ui_MainWindow(object):
         self.NextFrame.setText(_translate("MainWindow", "|▶"))
         self.BeforeFrame.setText(_translate("MainWindow", "◀|"))
         self.Before2DBox.setText(_translate("MainWindow", "◁|"))
-        self.NextFrame_2.setText(_translate("MainWindow", "|▷"))
+        self.Next2DBox.setText(_translate("MainWindow", "|▷"))
         self.menuFile.setTitle(_translate("MainWindow", "File"))
         self.OpenFile.setText(_translate("MainWindow", "開く"))
         self.OpenFile.setShortcut(_translate("MainWindow", "Ctrl+O"))
         self.SaveFile.setText(_translate("MainWindow", "保存"))
         self.SaveFile.setShortcut(_translate("MainWindow", "Ctrl+S"))
 
+    def OpenDialog(self):
+        # 画像ファイルの選択
+        self.image_file_name = QFileDialog.getOpenFileName(caption='Open file', directory='/home/hiroki/KITTI', filter="Image(*.png *.jpg *.jpeg)")
+        # 画像ファイルの読み込み
+        # img = cv2.imread(self.image_file_name[0])
+        # cv2.imshow("image", img)
+        pixmap = QtGui.QPixmap(self.image_file_name[0])
+        self.Image = QtGui.QImage(self.image_file_name[0])
+        self.ImageView.setPixmap(QtGui.QPixmap.fromImage(self.Image))
+        # file_numberの取得
+        self.file_number = int(os.path.splitext(os.path.basename(self.image_file_name[0]))[0])
+        # ディレクトリの取得
+        image_dir_name = os.path.dirname(self.image_file_name[0])
+        data_dir_name = os.path.join(image_dir_name)
+        # ポイントクラウドの読み込み
+        pointcloud_dir_name = os.path.join(data_dir_name, 'PointCloud')
+        pointcloud_file_name = os.path.splitext(os.path.basename(self.image_file_name[0]))[0]
+        self.pointcloud_file_name = os.path.join(pointcloud_dir_name, pointcloud_file_name, '.bin')
+        # pc_velo = np.fromfile(velo_filename, dtype=np.float32)
+        # pc_velo = pc_velo.reshape((-1, 4))
+        # 2DBoxの読み込み
+        label2d_dir_name = os.path.join(data_dir_name, 'label2d')
+        label2d_file_name = os.path.splitext(os.path.basename(self.image_file_name[0]))[0]
+        self.label2d_file_name = os.path.join(label2d_dir_name, label2d_file_name,'.txt')
+        # lines = [line.rstrip() for line in open(label2d_file_name)]
+        # pts_box2d = [int(line) for line in lines]
+        return True
 
 if __name__ == "__main__":
     import sys
